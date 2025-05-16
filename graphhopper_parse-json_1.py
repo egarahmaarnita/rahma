e2bf302456a240a3929cc4e28bb31d556729dcea
NetAcad DEVASC <changeme@example.com>
2025-05-16 07:37:40 +0000

    first commit

---
+++ graphhopper_parse-json_1.py
@@ -0,0 +1,15 @@
+import requests
+import urllib.parse
+
+geocode_url = "https://graphhopper.com/api/1/geocode?"
+route_url = "https://graphhopper.com/api/1/route?"
+loc1 = "Washington, D.C."
+loc2 = "Baltimore, Maryland"
+key = "8da30b1d-a3fa-482a-8990-99822e145f0f"
+
+url = geocode_url + urllib.parse.urlencode({"q":loc1, "limit": "1", "key":key})
+
+replydata = requests.get(url)
+json_data = replydata.json()
+json_status = replydata.status_code
+print(json_data)
